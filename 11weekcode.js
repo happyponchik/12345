2ec9364aaa17e79878f6a370bb59cb680b4e6ceb
happyponchik <106190544+happyponchik@users.noreply.github.com>
2022-05-24 23:10:10 +0300

    Create 11weekcode.js

---
+++ 11weekcode.js
@@ -0,0 +1,58 @@
+function init() {
+	 scene = new THREE.Scene();
+	 var WIDTH = window.innerWidth, HEIGHT = window.innerHeight;
+
+	 var renderer = new THREE.WebGLRenderer({canvas: document.getElementById('Canvas'), antialias:true});
+	renderer.setClearColor(0x333333);
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+	 document.body.appendChild(renderer.domElement);
+
+	 var camera=new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 20000);
+	camera.position.z = 200;
+
+	 scene.add(camera);
+
+	window.addEventListener('resize', function() {
+	var WIDTH = window.innerWidth, HEIGHT = window.innerHeight;
+	 renderer.setSize(WIDTH, HEIGHT);
+	 camera.aspect = WIDTH / HEIGHT;
+	 camera.updateProjectMatrix();
+	 });
+	
+	var lightOne= new THREE.AmbientLight(0xfffff, 0.5);
+	scene.add(lightOne);
+	
+	var lightTwo=new THREE.PointLight(0xfffff, 0.5);
+	scene.add(lightTwo);
+
+	 var objLoader = new THREE.OBJLoader();
+	objLoader.load("bed.obj", function(mesh){
+			mesh.traverse(function(node){
+				if(node instanceof THREE.Mesh){
+					node.castShadow = true;
+					node.receiveShadow = true;
+				}
+			})
+			scene.add(mesh);
+			mesh.position.set(-20, 50, -200);
+			mesh.rotation.y = -Math.PI/4;
+
+	});
+
+	requestAnimationFrame(render);
+
+	function render() {
+		renderer.render(scene, camera);
+		requestAnimationFrame(render);
+	}
+	 controls = new THREE.OrbitControls(camera,renderer.domElement);
+}
+
+function animate()
+{
+	 controls.update();
+	 renderer.render(scene, camera);
+	 requestAnimationFrame(animate);
+}
