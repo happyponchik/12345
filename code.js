a9c015570447b85f50a44787cb3ef5bed171a8d6
happyponchik <106190544+happyponchik@users.noreply.github.com>
2022-05-25 00:30:11 +0300

    Update code.js

--- code.js
+++ code.js
@@ -10,7 +10,8 @@ function init() {
 	 document.body.appendChild(renderer.domElement);
 
 	 var camera=new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 20000);
-	 camera.position.z = 500;
+	 camera.position.z = 200;
+	camera.position.x = 200;
 
 	 scene.add(camera);
 
