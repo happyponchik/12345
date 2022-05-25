50eedf5ab117f9bb10297c5dd1cdc72556e5be43
happyponchik <106190544+happyponchik@users.noreply.github.com>
2022-05-25 12:49:23 +0300

    Update 11code.js

--- 11code.js
+++ 11code.js
@@ -10,7 +10,7 @@ function init() {
 	 document.body.appendChild(renderer.domElement);
 
 	 var camera=new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 20000);
-	 camera.position.z = 500;
+	 camera.position.z = 300;
 	camera.position.y = 200;
 	camera.position.x = 200;
 
